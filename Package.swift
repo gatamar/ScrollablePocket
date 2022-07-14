fdefa0ec2b0b605d5595483fc11cc18dca453135
Olha Pavliuk <olia.pavliuk@gmail.com>
2022-07-14 16:00:17 +0200

    add a package

---
+++ Package.swift
@@ -0,0 +1,14 @@
+import PackageDescription
+
+let package = Package(
+    name: "scrollable_pocket",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        .library(
+            name: "scrollable_pocket",
+            targets: ["scrollable-pocket"]
+        )
+    ]
+)
